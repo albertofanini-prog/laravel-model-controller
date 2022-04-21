56c4787fc0e11f4b135db4fb71dfc86857c55602
albertofanini-prog <albertofanini@outlook.com>
2022-04-21 18:57:54 +0200

    Show movies title

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Movie;
+
 class MovieController extends Controller
 {
-    //
+    public function index(){
+        $movies = Movie::all();
+
+        return view('home', compact('movies'));
+    }
 }
