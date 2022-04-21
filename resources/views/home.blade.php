1d176a5c6cd1fc26d7550536d71fa710f95367e7
albertofanini-prog <albertofanini@outlook.com>
2022-04-21 19:30:29 +0200

    Correzioni Scss

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,12 +8,28 @@
     <title>Film</title>
 </head>
 <body>
-    <ul class="movies_list">
-        @foreach ($movies as $movie)
-            <li class="movie">
-                {{$movie->title}}
-            </li>
-        @endforeach
-    </ul>
+    <div class="movies_wrapper">
+        <ul class="movies_list">
+            @foreach ($movies as $movie)
+                <li class="movie">
+                    <h1>
+                        Titolo: {{$movie->title}}
+                    </h1>
+                    <h4>
+                        Titolo originale: {{$movie->original_title}}
+                    </h4>
+                    <h4>
+                        Nazionalità: {{$movie->nationality}}
+                    </h4>
+                    <h4>
+                        Data di rilascio: {{$movie->date}}
+                    </h4>
+                    <h4>
+                        Voto medio: {{$movie->vote}}/10
+                    </h4>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 </body>
 </html>
