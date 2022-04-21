9e71ed297d978172a92c32bbf495428b92aa2231
albertofanini-prog <albertofanini@outlook.com>
2022-04-21 19:17:07 +0200

    npm install, inclusione scss - Last commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,12 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ URL('./css/app.css')}}">
     <title>Film</title>
 </head>
 <body>
-    <ul>
+    <ul class="movies_list">
         @foreach ($movies as $movie)
-            <li>
+            <li class="movie">
                 {{$movie->title}}
             </li>
         @endforeach
